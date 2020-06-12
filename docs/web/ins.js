57218cb400470dd78cb2d0455c21050581fb602e
dolyw <1107224733@qq.com>
2020-06-12 20:57:32 +0800

    调整badjs，调整相册

--- docs/web/ins.js
+++ docs/web/ins.js
@@ -111,16 +111,26 @@
       }
     };
     var render = function render(res) {
+
+      var myPicList = [];
+      var myPic = {};
+      for (var x = 0, picListlen = picList.length; x < picListlen; x++) {
+        var tempDate = picList[x].alt.slice(0, 6);
+        console.log(tempDate);
+      }
+
       var ulTmpl = "";
       for (var j = 0, len2 = res.list.length; j < len2; j++) {
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
-          var src = 'http://litten.me/ins/' + data.link[i];
+          // var minSrc = 'http://litten.me/ins-min/' + data.link[i] + '.min.jpg';
+          // var src = 'http://litten.me/ins/' + data.link[i];
+          var minSrc = data.link[i];
+          var src = data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '.jpg';
+          // src += '.jpg';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
                 <a href="' + src + '" itemprop="contentUrl" data-size="640x640" data-type="' + type + '" data-target="' + target + '">\
